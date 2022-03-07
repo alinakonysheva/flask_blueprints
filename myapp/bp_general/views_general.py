899ad36217626c414122179b6212e546c2a8e8e7
Alina Konysheva <konysheva.alina@gmail.com>
2022-03-07 22:08:39 +0100

    added page books

--- myapp/bp_general/views_general.py
+++ myapp/bp_general/views_general.py
@@ -1,13 +1,20 @@
 from flask import render_template, abort, redirect, flash, url_for, current_app, Blueprint
 from myapp import db
-from bp_general.controller_general import ControllerBook
+from bp_general.controller_general import ControllerBook, ControllerEBook
 
-bp_general_app = Blueprint('bp_general', __name__,  cli_group="db")
+bp_general_app = Blueprint('bp_general', __name__, cli_group="db")
+
 
 @bp_general_app.route('/')
 def do_home():
     controller = ControllerBook(db.session)
     return render_template('general/home.html', name='some name', ids=controller.get_all_ids())
+
+
+@bp_general_app.route('/books')
+def do_books():
+    controller = ControllerEBook(db.session)
+    return render_template('general/books.html', books=controller.get_all_ebook())
 
 
 def do_not_found(error):
